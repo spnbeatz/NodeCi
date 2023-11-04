437f0543c752b53d4e4a19839babb386eddd0df3
spnbeatz <111923195+spnbeatz@users.noreply.github.com>
2023-11-04 15:52:01 +0100

    Travis config

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,9 @@
 module.exports = {
   googleClientID:
-    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
-  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
-  cookieKey: '123123123',
+    "70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com",
+  googleClientSecret: "8mkniDQOqacXtlRD3gA4n2az",
+  mongoURI:
+    'mongodb+srv://spnbeatz:qwertySPN1@cluster0.ygeesry.mongodb.net/advnode?retryWrites=true&w=majority',
+  cookieKey: "123123123",
+  redisUrl: 'redis://127.0.0.1:6379'
 };
